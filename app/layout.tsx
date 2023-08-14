fb2d1fb5ede1f99b1f78c7e6ce0a4cbf245e6153
Bastieno <francis.nduamaka@fulfil.io>
2023-08-14 15:38:59 +0100

    dynamically generate metadata for the pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,37 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { headers } from 'next/headers';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'Settings page',
-  description: 'Settings page of a imaginary fintech app',
+export async function generateMetadata(): Promise<Metadata> {
+  const headersList = headers();
+  const pathname = headersList.get('x-invoke-path') || 'Untitled UI';
+
+  // Split pathname into segments and remove empty segments
+  const pathSegments = pathname.split('/').filter((segment) => segment !== '');
+  // Capitalize the first letter of each segment
+  const titleSegments = pathSegments.map(
+    (segment) => segment.charAt(0).toUpperCase() + segment.slice(1)
+  );
+  // Join segments with a separator
+  const title = titleSegments.join(' › ');
+
+  return {
+    title: `${title} | Untitled UI`,
+    description: `${title} page of an imaginary fintech app`,
+  };
 }
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
